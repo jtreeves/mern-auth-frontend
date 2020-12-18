42f0855ac90b4a7bddbfb7bed25cbeb9056579d5
jtreeves <jackson.t.reeves@gmail.com>
2020-12-18 16:33:42 -0500

    [Login] Copy Signup with minor deletions and changes

---
+++ src/components/Login.js
@@ -0,0 +1,48 @@
+// require('dotenv').config()
+import React, { useState, useEffect } from 'react'
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
+import jwt_decode from 'jwt-decode'
+import setAuthToken from '../utils/setAuthToken'
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
+
+const Login = (props) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    if (props.user) return <Redirect to="/profile" />
+
+    return (
+        <div className="row mt-4">
+            <div className="col-md-7 offset-md-3">
+                <div className="card card-body">
+                    <h2 className="py-2">Log In</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" name="email" value={email} onChange={handleEmail} className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" value={password} onChange={handlePassword} className="form-control" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
