289924e712ca0309157660814588d84b8b153fe3
jtreeves <jackson.t.reeves@gmail.com>
2020-12-20 14:39:21 -0500

    [Profile] Delete unnecessary code

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,28 +3,12 @@ import { Link } from 'react-router-dom'
 
 // Create function
 function Profile(props) {
-    // Pull current user data from props
-    const userData = props.user
-    ? (<div>
-        <h1>Profile</h1>
-        <p><strong>Name:</strong> {props.user.name}</p> 
-        <p><strong>Email:</strong> {props.user.email}</p> 
-        <p><strong>ID:</strong> {props.user.id}</p> 
-    </div>)
-    : <h4>Loading...</h4>
-
-    // Display error message if not logged in
-    const errorDiv = () => {
-        return (
-            <div className="text-center pt-4">
-                <h3>Please <Link to="/login">login</Link> to view this page</h3>
-            </div>
-        )
-    }
-    
     return (
         <div>
-            { props.user ? userData : errorDiv() }
+            <h1>Profile</h1>
+            <p><strong>Name:</strong> {props.user.name}</p> 
+            <p><strong>Email:</strong> {props.user.email}</p> 
+            <p><strong>ID:</strong> {props.user.id}</p>
         </div>
     )
 }
